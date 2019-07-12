c24ddd9dffa3fe4f626a5e6c5f8e8c1f90493d0c
fakhrinf <fakhrinf@hotmail.com>
2019-07-12 09:15:56 +0700

    [UPDATE] play with keyboard extras

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Markup = require('telegraf/markup');
 const Extra = require('telegraf/extra');
 const bot = new Telbot(process.env.BOT_TOKEN)
 
-bot.use(Telbot.log());
+// bot.use(Telbot.log());
 
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
@@ -12,16 +12,23 @@ bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 bot.command('id', (ctx) => {
     const info = ctx.from;
-    return ctx.reply(`Hello ${info['first_name']},\nYour id is: ${info['id']}`);
+    return ctx.reply(`Hello ${info['first_name']},\nYour id is: ${info['id']}`, Extra.markup(
+        Markup.removeKeyboard()
+    ));
 });
 
 bot.command('sync', (ctx) => {
-    return ctx.reply('Sync your phone number', Extra.markup((markup) => {
-        return markup.resize().keyboard([
-            markup.contactRequestButton('Authorized'),
-            markup.callbackButton('Cancel', 'cancel')
-        ]).oneTime();
-    }));
+    return ctx.reply('Sync your phone number', Extra.markup(
+        Markup.keyboard([
+            Markup.contactRequestButton('Autorized'),
+            Markup.callbackButton('Cancel')
+        ]).resize().oneTime(true)
+    ));
+});
+
+bot.on('contact', (ctx) => {
+    const data = ctx.message.contact;
+    ctx.reply(`Your phone number is: ${data.phone_number}`, Extra.markup(Markup.removeKeyboard()));
 });
 
 bot.launch()
